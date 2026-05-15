56e8808c847f73b9613b853465048bb2b1833019
Deploy <deploy@fieldserviceit.com>
2026-05-15 09:28:49 -0400

    Add binaryTargets for openssl-3.0.x to Prisma schema

--- dist/common/guards/jwt-auth.guard.d.ts
+++ dist/common/guards/jwt-auth.guard.d.ts
@@ -4,7 +4,7 @@ declare const JwtAuthGuard_base: import("@nestjs/passport").Type<import("@nestjs
 export declare class JwtAuthGuard extends JwtAuthGuard_base {
     private reflector;
     constructor(reflector: Reflector);
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | import("rxjs").Observable<boolean>;
+    canActivate(context: ExecutionContext): any;
     handleRequest(err: any, user: any): any;
 }
 export {};
