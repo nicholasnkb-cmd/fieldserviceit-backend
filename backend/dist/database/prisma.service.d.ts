1b0eab3597d401dd13022000317368c15edc52ff
Deploy <deploy@fieldserviceit.com>
2026-05-15 19:46:24 -0400

    fix: remove broken / from PrismaService and fix duplicate permission.findMany

--- backend/dist/database/prisma.service.d.ts
+++ backend/dist/database/prisma.service.d.ts
@@ -1,7 +1,4 @@
-import { OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { DatabaseService } from './database.service';
-export declare class PrismaService extends DatabaseService implements OnModuleInit, OnModuleDestroy {
-    private readonly logger;
-    onModuleInit(): Promise<void>;
-    onModuleDestroy(): Promise<void>;
+export declare class PrismaService extends DatabaseService {
+    constructor();
 }
--- backend/dist/database/prisma.service.js
+++ backend/dist/database/prisma.service.js
@@ -5,31 +5,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var PrismaService_1;
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PrismaService = void 0;
 const common_1 = require("@nestjs/common");
 const database_service_1 = require("./database.service");
-let PrismaService = PrismaService_1 = class PrismaService extends database_service_1.DatabaseService {
+let PrismaService = class PrismaService extends database_service_1.DatabaseService {
     constructor() {
-        super(...arguments);
-        this.logger = new common_1.Logger(PrismaService_1.name);
-    }
-    async onModuleInit() {
-        try {
-            await this.$connect();
-            this.logger.log('Database connected (mysql2)');
-        }
-        catch (err) {
-            this.logger.warn('Database unavailable: ' + (err instanceof Error ? err.message : String(err)));
-        }
-    }
-    async onModuleDestroy() {
-        await this.$disconnect();
+        super();
     }
 };
 exports.PrismaService = PrismaService;
-exports.PrismaService = PrismaService = PrismaService_1 = __decorate([
-    (0, common_1.Injectable)()
+exports.PrismaService = PrismaService = __decorate([
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [])
 ], PrismaService);
 //# sourceMappingURL=prisma.service.js.map
